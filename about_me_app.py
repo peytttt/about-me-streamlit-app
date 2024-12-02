ae0d44467c45c009fc7d138aeab3f482d029e76d
peytttt <jolliefaithj@gmail.com>
2024-12-02 22:48:47 +0800

    Update about_me_app.py

--- about_me_app.py
+++ about_me_app.py
@@ -4,7 +4,7 @@ import streamlit as st
 st.title("About Jollie Faith S. Jimenez")
 
 # Add image from GitHub
-st.image("https://raw.githubusercontent.com/peytttt/about-me-streamlit-app/images/me.jpg", caption="Jollie Faith S. Jimenez", use_container_width=True)
+st.image("https://raw.githubusercontent.com/peytttt/about-me-streamlit-app/main/images/faith.jpg", caption="Jollie Faith S. Jimenez", use_container_width=True)
 
 
 # Description
