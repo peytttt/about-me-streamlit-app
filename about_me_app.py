51d7f64ab2f711086d1325b4e7de19384d615748
peytttt <jolliefaithj@gmail.com>
2024-12-05 11:31:11 +0800

    Update about_me_app.py

--- about_me_app.py
+++ about_me_app.py
@@ -1,9 +1,7 @@
 import streamlit as st
 
-# Set the title
 st.title("MY BLOG")
 
-# Customizing the page's background and text styles
 st.markdown(
     """
     <style>
@@ -43,35 +41,30 @@ st.markdown(
     unsafe_allow_html=True
 )
 
-# Add image from GitHub with specified size
 st.image(
     "https://raw.githubusercontent.com/peytttt/about-me-streamlit-app/main/images/me.jpg", 
     use_container_width=True
 )
 
-# Personal Details text after the photo
 st.write("**NAME:** Jollie Faith S. Jimenez")
 st.write("**BIRTHDAY:** July 15, 2005")
 st.write("**COURSE:** BS Computer Engineering (BSCPE)")
 st.write("**SECTION:** 1B")
 
-# Description
 st.subheader("Hi there!")
 st.write("My name is Jollie Faith S. Jimenez, a 1st-year Computer Engineering student at SNSU.")
 
-# About Me Section
 st.header("About Me")
 st.write("""
 Hello, I'm Jollie Faith S. Jimenez, 19 years old. I was born in San Fernando, Pampanga, and I'm proud to be a Kapampangan, 
 even though I don't speak the language. We moved to Surigao City when I was 2 years old, and I’ve lived here since. 
 
 I have 2 cats named Tobby and Ginger. Tobby is inspired by Dobby, the house-elf from Harry Potter, while Ginger got his name 
-because of his orange fur. My family is religious, and I’ve been serving the Catholic Church since I was a little kid. 
+because of his orange fur. My family is religious, and I’ve served the Catholic Church since I was a little kid. 
 
-I also adore stuffed toys and can’t sleep without them. I’ve had one since childhood, and it still brings me comfort and assurance.
+I also adore stuffed toys and can’t sleep without them. I’ve had one since childhood, which still brings comfort and assurance.
 """)
 
-# Hobbies and Interests Section
 st.header("Hobbies and Interests")
 st.write("""
 - Watching movies (especially Harry Potter).
@@ -80,10 +73,8 @@ st.write("""
 - Watching documentaries about animals, history, and real-life investigation cases.
 """)
 
-# Certificates Section
 st.header("Certificates")
 
-# Display certificates statically
 st.subheader("My Certificates")
 certificates = [
     "Participation Certificate - SHS Expo 2023-2024"
@@ -95,5 +86,4 @@ if certificates:
 else:
     st.write("No certificates available.")
 
-# Footer
 st.markdown("<footer>© 2024 Jollie Faith S. Jimenez</footer>", unsafe_allow_html=True)
